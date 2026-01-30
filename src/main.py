0474b8d60d5d6de81d58bf79c4477eead6fd90c8
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2026-01-30 11:05:12 -0500

    Add framework skeleton to main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
+# This is used as a framework for the machine learning model pipeline
+def load():
+  pass
+def preprocess():
+  pass
+def train():
+  pass
+def validate():
+  pass
+def aggregate(): # Used for aggregating the final statistics
+  pass
 
+if __name__ == "main":
+  # load()
+  # TODO: Have the framework automatically generate a log and store it in the `outputs` folder,
+  # uniquely named by the run datetime, and have a copy of its hyperparameters via `param.py`.
