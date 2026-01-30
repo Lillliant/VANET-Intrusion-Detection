ee755dee8e79324bd243b63e63110070d96b35ca
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2026-01-30 13:16:47 -0500

    Create param.py for hyperparameter configuration
    Add configuration file for machine learning hyperparameters.

---
+++ src/param.py
@@ -0,0 +1,10 @@
+# This file will act as a configuration file listing the hyperparameters for the machine learning framework.
+"""
+# This is an example
+model: ['LDA', '...'],
+hyperparameters: {
+  'LDA': {
+      ...
+  }
+}
+"""
