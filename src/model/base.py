59039043d0f7f951a9a3de0704625d4ce9f3b243
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2026-01-30 11:02:13 -0500

    Expand Base class
    Added preprocess and evaluate methods to the Base class.

--- src/model/base.py
+++ src/model/base.py
@@ -1,11 +1,20 @@
 class Base:
   # A base class for all machine learning models
+  # This is used as a easy method for integrating different machine learning frameworks
+  # from different libraries (e.g., PyTorch, Scikit-learn).
+  
   def __init__(self, name, model):
     self.name = name
     self.model = model
+
+  def preprocess(self):
+    pass
 
   def train(self):
     pass
 
   def predict(self):
+    pass
+
+  def evaluate(self):
     pass
