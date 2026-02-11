46bebd62217a7198c69417ae1ef52408d0c4bda4
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2026-02-11 13:24:31 -0500

    Temporary fix before code refactor

--- src/util/metrics.py
+++ src/util/metrics.py
@@ -11,6 +11,6 @@ def get_scorers(metric_names, multiclass=False):
         'accuracy': accuracy_score,
         'precision': lambda y_true, y_pred: precision_score(y_true, y_pred, average='weighted' if multiclass else 'binary'),
         'recall': lambda y_true, y_pred: recall_score(y_true, y_pred, average='weighted' if multiclass else 'binary'),
-        'f1': lambda y_true, y_pred: f1_score(y_true, y_pred, average='weighted' if multiclass else 'binary')
+        'f1_score': lambda y_true, y_pred: f1_score(y_true, y_pred, average='weighted' if multiclass else 'binary')
     }
     return {m: scorer_map[m] for m in metric_names if m in scorer_map}
