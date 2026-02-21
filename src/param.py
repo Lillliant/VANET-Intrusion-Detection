bf683d70103d1d83b4c7287e4451b86fe5a051c3
Asma Altumi <116416419+asma305305@users.noreply.github.com>
2026-02-21 15:04:43 -0500

    Change class parameter from 15 to 14

--- src/param.py
+++ src/param.py
@@ -79,7 +79,7 @@ GRID_PARAMS = {
 DATA_PARAMS = {
     'samples': None, # Use all samples; Otherwise, specify a number (e.g., 10000)
 	'test_size': 0.15, # Percentage based on samples as defined above
-    'class': 15, # Examine only one class; Otherwise, examine all classes (set to None)
+    'class': 14, # Examine only one class; Otherwise, examine all classes (set to None)
     'validation_size': 0.15,
 	'random_state': 42,
 }
